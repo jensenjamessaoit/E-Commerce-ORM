7c5628a3cee0ca16ebc114140d602917c74ed818
jj <saoitjensen@gmail.com>
2023-09-19 23:48:22 -0600

    finished models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.belongsTo(Product, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  trhough: ProductTag,
+});
 
 module.exports = {
   Product,
