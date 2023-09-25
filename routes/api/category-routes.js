f09784159037418ff9541835cd0c659b15611017
jj <saoitjensen@gmail.com>
2023-09-25 00:24:05 -0600

    updated routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,7 +28,6 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({message: 'No category found with that id.'});
       return
     }
-
     res.status(200).json(categoryData);
   } catch(err) {
     res.status(500).json(err);
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,22 +3,63 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product}, { model: ProductTag }]
+    })
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk( req.params.id, {
+      include: [{ model: Product}, { model: ProductTag }]
+    })
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name, 
+      },
+      {
+        where: req.params.id,
+      }
+    );
+
+    if(!tagData) {
+      res.status(404).json({ message: 'No tag with that id found'});
+    }
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500);
+  }
 });
 
 router.delete('/:id', (req, res) => {
